d7f1c3b4316687a45259a0fee1d20a2c28b6af0f
Sagnik Pal <131534396+SagnikPal-7@users.noreply.github.com>
2025-09-15 02:34:10 +0530

    About page done

--- frontend/src/components/RelatedDoctor.jsx
+++ frontend/src/components/RelatedDoctor.jsx
@@ -21,7 +21,7 @@ const RelatedDoctor = ({ docId, speciality }) => {
     <div className="flex  flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
       <h1 className="text-3xl font-medium">Related Doctors</h1>
       <p className="sm:w-1/3 text-center text-sm">
-        Simply browse through same extensive list of trusted doctors.
+        Simply browse doctors with same speciality
       </p>
 
       <div className="w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0">
