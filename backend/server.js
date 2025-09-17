0abe9894299552bd265fbe5f7cbbdec1fe2b0e27
Sagnik Pal <131534396+SagnikPal-7@users.noreply.github.com>
2025-09-17 22:30:15 +0530

    backend started

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("API WORKING great!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
