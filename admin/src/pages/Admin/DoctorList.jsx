3533aa5a2002c8f5777b6b607f56f25576ade432
Sagnik Pal <131534396+SagnikPal-7@users.noreply.github.com>
2025-09-23 04:34:15 +0530

    fixed

--- admin/src/pages/Admin/DoctorList.jsx
+++ admin/src/pages/Admin/DoctorList.jsx
@@ -13,7 +13,7 @@ const DoctorList = () => {
   return (
     <div className="w-3/4 flex flex-col items-center pt-8 px-8 max-h-[90vh] overflow-y-auto">
       <h1 className="text-2xl font-semibold mb-6 self-start">All Doctors</h1>
-      <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8 justify-items-center pr-5">
+      <div className="ml-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-20 justify-items-center pr-5">
         {doctors.map((item, index) => (
           <div
             className="bg-white border border-indigo-100 rounded-2xl shadow-md w-56 min-h-[320px] flex flex-col items-center overflow-hidden cursor-pointer group transition-all duration-300 hover:shadow-lg mx-3"
