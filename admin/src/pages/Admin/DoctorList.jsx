f09f02dab13bce8538c1f2e2b3c5fa4730ef9ddb
Sagnik Pal <131534396+SagnikPal-7@users.noreply.github.com>
2025-09-23 04:47:23 +0530

    final fix done

--- admin/src/pages/Admin/DoctorList.jsx
+++ admin/src/pages/Admin/DoctorList.jsx
@@ -11,15 +11,15 @@ const DoctorList = () => {
   }, [aToken]);
 
   return (
-    <div className="w-3/4 flex flex-col items-center pt-8 px-8 max-h-[90vh] overflow-y-auto">
+    <div className="w-4/5 flex flex-col items-center pt-8 px-8 max-h-[90vh] overflow-y-auto">
       <h1 className="text-2xl font-semibold mb-6 self-start">All Doctors</h1>
       <div className="ml-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-20 justify-items-center pr-5">
         {doctors.map((item, index) => (
           <div
-            className="bg-white border border-indigo-100 rounded-2xl shadow-md w-56 min-h-[320px] flex flex-col items-center overflow-hidden cursor-pointer group transition-all duration-300 hover:shadow-lg mx-3"
+            className="bg-white border border-indigo-100 rounded-2xl shadow-md w-56 min-h-[320px] flex flex-col items-center overflow-hidden cursor-pointer group transition-all duration-300 hover:shadow-lg mx-3 mb-[-35px]"
             key={index}
           >
-            <div className="w-full h-48 flex items-center justify-center bg-indigo-50 group-hover:bg-primary transition-all duration-500">
+            <div className="w-full h-48 flex items-center justify-center bg-indigo-50 group-hover:bg-indigo-400 transition-all duration-500">
               <img
                 className="object-cover h-52 w-40"
                 src={item.image}
