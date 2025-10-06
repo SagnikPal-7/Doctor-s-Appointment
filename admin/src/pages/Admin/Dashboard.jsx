01dcb10302c2f820f24dbd29b705f8f91efcc15e
Sagnik Pal <131534396+SagnikPal-7@users.noreply.github.com>
2025-10-06 10:37:34 +0530

    dashboard done

--- admin/src/pages/Admin/Dashboard.jsx
+++ admin/src/pages/Admin/Dashboard.jsx
@@ -1,10 +1,13 @@
 import React, { useContext, useEffect } from "react";
 import { AdminContext } from "../../context/AdminContext";
+import { AppContext } from "../../context/AppContext";
 import { assets } from "../../assets/assets";
 
 const Dashboard = () => {
   const { aToken, getDashData, dashData, cancelAppointment } =
     useContext(AdminContext);
+
+  const { slotDateFormat } = useContext(AppContext);
 
   useEffect(() => {
     if (aToken) {
@@ -16,28 +19,79 @@ const Dashboard = () => {
     dashData && (
       <div className="m-5">
         <div className="flex flex-wrap gap-3">
-          <div className="flex items-center gap-2 bg-white p-4 min-w-52 rounded-lg border-2 border-gray-100 cursor-pointer hover:scale-105 hover:bg-purple-300 transition-all">
-            <img src={assets.doctor_icon} alt="" />
+          <div className="flex items-center gap-2 bg-white p-4 w-56 rounded-lg border-2 border-gray-100 cursor-pointer hover:scale-105 hover:bg-purple-300 transition-all">
+            <img className="w-14" src={assets.doctor_icon} alt="" />
             <div>
-              <p>{dashData.doctors}</p>
-              <p>Doctors</p>
+              <p className="text-xl font-semibold text-gray-600">
+                {dashData.doctors}
+              </p>
+              <p className="text-gray-500">Doctors</p>
             </div>
           </div>
 
-          <div className="flex items-center gap-2 bg-white p-4 min-w-52 rounded-lg border-2 border-gray-100 cursor-pointer hover:scale-105 hover:bg-purple-300 transition-all">
-            <img src={assets.appointments_icon} alt="" />
+          <div className="flex items-center gap-2 bg-white p-4 w-56 rounded-lg border-2 border-gray-100 cursor-pointer hover:scale-105 hover:bg-purple-300 transition-all">
+            <img className="w-14" src={assets.appointments_icon} alt="" />
             <div>
-              <p>{dashData.appointments}</p>
-              <p>Appointments</p>
+              <p className="text-xl font-semibold text-gray-600">
+                {dashData.appointments}
+              </p>
+              <p className="text-gray-500">Appointments</p>
             </div>
           </div>
 
-          <div className="flex items-center gap-2 bg-white p-4 min-w-52 rounded-lg border-2 border-gray-100 cursor-pointer hover:scale-105 hover:bg-purple-300 transition-all">
-            <img src={assets.patients_icon} alt="" />
+          <div className="flex items-center gap-2 bg-white p-4 w-56 rounded-lg border-2 border-gray-100 cursor-pointer hover:scale-105 hover:bg-purple-300 transition-all">
+            <img className="w-14" src={assets.patients_icon} alt="" />
             <div>
-              <p>{dashData.patients}</p>
-              <p>Patients</p>
+              <p className="text-xl font-semibold text-gray-600">
+                {dashData.users}
+              </p>
+              <p className="text-gray-500">Patients</p>
             </div>
+          </div>
+        </div>
+
+        <div className="bg-white">
+          <div className="flex items-center gap-2.5 px-4 py-4 mt-10 rounded-t border">
+            <img src={assets.list_icon} alt="" />
+            <p className="font-semibold">Latest Bookings</p>
+          </div>
+
+          <div className="pt-4 border border-t-0">
+            {dashData.latestAppointments.map((item, index) => (
+              <div
+                className="flex items-center px-6 py-3 gap-3 hover:bg-gray-100"
+                key={index}
+              >
+                <img
+                  className="w-12 h-12 rounded-full"
+                  src={item.docData.image}
+                  alt=""
+                />
+                <div className="flex-1 text-sm">
+                  <p className="text-gray-800 font-medium">
+                    {item.docData.name}
+                  </p>
+                  <p className="text-gray-600">
+                    {slotDateFormat(item.slotDate)}
+                  </p>
+                </div>
+
+                {item.payment ? (
+                  <p className="text-green-500 text-xs font-medium "> Paid</p>
+                ) : item.cancelled ? (
+                  <p className="text-red-400 text-xs font-medium cursor-not-allowed">
+                    Cancelled
+                  </p>
+                ) : (
+                  <img
+                    onClick={() => cancelAppointment(item._id)}
+                    className="w-10 cursor-pointer"
+                    src={assets.cancel_icon}
+                    alt=""
+                  />
+                )}
+              </div>
+            ))}
           </div>
         </div>
       </div>
